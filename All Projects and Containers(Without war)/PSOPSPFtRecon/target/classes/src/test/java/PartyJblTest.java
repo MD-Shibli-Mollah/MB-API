da5e39e5b183beb98cdfc346e163cabbafdb17e7
MD SHIBLI MOLLAH <md.ta.shibli@gmail.com>
2024-05-18 15:36:37 +0600

    PSO/PSP FT Recon

---
+++ All Projects and Containers(Without war)/PSOPSPFtRecon/target/classes/src/test/java/PartyJblTest.java
@@ -0,0 +1,85 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.camel.Exchange;
+import org.junit.Test;
+
+import org.junit.BeforeClass;
+import com.temenos.irf.testsupport.*;
+
+
+public class PartyJblTest extends CamelRouteTest
+{
+
+ private static CamelUnitTestHelper helper;
+@BeforeClass
+public static void init() {
+   helper = new CamelUnitTestHelper("party-jbl.service.v1.0.0", "./src/test/resources/test-data/");
+}
+@Test
+public void testgetFtReconciliation() {
+  String requestName = "getFtReconciliation";
+helper.getTestSupportConnectionFactory().setRequestName(requestName);
+helper.getTestSupportConnectionFactory().setRecording(true);
+Map<String, Object> headers = new HashMap<>();
+headers.put("id", "");// THIS IS REQUIRED
+headers.put("reference", "");
+headers.put("page_size", "");
+headers.put("page_start", "");
+headers.put("page_token", "");
+headers.put("credentials", "");
+headers.put("companyId", "");
+headers.put("deviceId", "");
+headers.put("userRole", "");
+headers.put("disablePagination", "");
+headers.put(Exchange.HTTP_METHOD, "GET");
+String body = "";
+Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-jbl.getFtReconciliation", body, headers);
+ Map<String, List<String>> testResults = helper.checkResponse(requestName, result);
+super.checkPaths(testResults);
+}
+@Test
+public void testgetFtReconciliation() {
+  String requestName = "getFtReconciliation";
+helper.getTestSupportConnectionFactory().setRequestName(requestName);
+helper.getTestSupportConnectionFactory().setRecording(true);
+Map<String, Object> headers = new HashMap<>();
+headers.put("id", "");// THIS IS REQUIRED
+headers.put("reference", "");
+headers.put("page_size", "");
+headers.put("page_start", "");
+headers.put("page_token", "");
+headers.put("credentials", "");
+headers.put("companyId", "");
+headers.put("deviceId", "");
+headers.put("userRole", "");
+headers.put("disablePagination", "");
+headers.put(Exchange.HTTP_METHOD, "GET");
+String body = "";
+Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-jbl.getFtReconciliation", body, headers);
+ Map<String, List<String>> testResults = helper.checkResponse(requestName, result);
+super.checkPaths(testResults);
+}
+@Test
+public void testgetFtReconciliation() {
+  String requestName = "getFtReconciliation";
+helper.getTestSupportConnectionFactory().setRequestName(requestName);
+helper.getTestSupportConnectionFactory().setRecording(true);
+Map<String, Object> headers = new HashMap<>();
+headers.put("id", "");// THIS IS REQUIRED
+headers.put("reference", "");
+headers.put("page_size", "");
+headers.put("page_start", "");
+headers.put("page_token", "");
+headers.put("credentials", "");
+headers.put("companyId", "");
+headers.put("deviceId", "");
+headers.put("userRole", "");
+headers.put("disablePagination", "");
+headers.put(Exchange.HTTP_METHOD, "GET");
+String body = "";
+Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-jbl.getFtReconciliation", body, headers);
+ Map<String, List<String>> testResults = helper.checkResponse(requestName, result);
+super.checkPaths(testResults);
+}
+}
