7c22ecc21cc864b7faec5debbd7e955417b2d332
MD SHIBLI MOLLAH <md.ta.shibli@gmail.com>
2024-05-18 16:23:36 +0600

    PSO/PSP FT with charge amount and vat amount

--- All API Resources/service-PSOPSP-FT-WITH-VATCHG-Resource/src/test/java/PartyPaymentsTest.java
+++ All API Resources/service-PSOPSP-FT-WITH-VATCHG-Resource/src/test/java/PartyPaymentsTest.java
@@ -8,74 +8,62 @@ import org.junit.BeforeClass;
 import com.temenos.irf.testsupport.*;
 
 
-public class PartyJblTest extends CamelRouteTest
+public class PartyPaymentsTest extends CamelRouteTest
 {
 
  private static CamelUnitTestHelper helper;
 @BeforeClass
 public static void init() {
-   helper = new CamelUnitTestHelper("party-jbl.service.v1.0.0", "./src/test/resources/test-data/");
+   helper = new CamelUnitTestHelper("party-payments.service.1.0.0", "./src/test/resources/test-data/");
 }
 @Test
-public void testgetFtReconciliation() {
-  String requestName = "getFtReconciliation";
+public void testcreatePsopspFtWithChgVat() {
+  String requestName = "createPsopspFtWithChgVat";
 helper.getTestSupportConnectionFactory().setRequestName(requestName);
 helper.getTestSupportConnectionFactory().setRecording(true);
 Map<String, Object> headers = new HashMap<>();
-headers.put("id", "");// THIS IS REQUIRED
-headers.put("page_size", "");
-headers.put("page_start", "");
-headers.put("page_token", "");
+headers.put("validate_only", "");
 headers.put("credentials", "");
 headers.put("companyId", "");
 headers.put("deviceId", "");
 headers.put("userRole", "");
-headers.put("disablePagination", "");
-headers.put(Exchange.HTTP_METHOD, "GET");
-String body = "";
-Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-jbl.getFtReconciliation", body, headers);
+headers.put(Exchange.HTTP_METHOD, "POST");
+String body = ""; // THIS IS REQUIRED
+Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-payments.createPsopspFtWithChgVat", body, headers);
  Map<String, List<String>> testResults = helper.checkResponse(requestName, result);
 super.checkPaths(testResults);
 }
 @Test
-public void testgetFtReconciliation() {
-  String requestName = "getFtReconciliation";
+public void testcreatePsopspFtWithChgVat() {
+  String requestName = "createPsopspFtWithChgVat";
 helper.getTestSupportConnectionFactory().setRequestName(requestName);
 helper.getTestSupportConnectionFactory().setRecording(true);
 Map<String, Object> headers = new HashMap<>();
-headers.put("id", "");// THIS IS REQUIRED
-headers.put("page_size", "");
-headers.put("page_start", "");
-headers.put("page_token", "");
+headers.put("validate_only", "");
 headers.put("credentials", "");
 headers.put("companyId", "");
 headers.put("deviceId", "");
 headers.put("userRole", "");
-headers.put("disablePagination", "");
-headers.put(Exchange.HTTP_METHOD, "GET");
-String body = "";
-Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-jbl.getFtReconciliation", body, headers);
+headers.put(Exchange.HTTP_METHOD, "POST");
+String body = ""; // THIS IS REQUIRED
+Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-payments.createPsopspFtWithChgVat", body, headers);
  Map<String, List<String>> testResults = helper.checkResponse(requestName, result);
 super.checkPaths(testResults);
 }
 @Test
-public void testgetFtReconciliation() {
-  String requestName = "getFtReconciliation";
+public void testcreatePsopspFtWithChgVat() {
+  String requestName = "createPsopspFtWithChgVat";
 helper.getTestSupportConnectionFactory().setRequestName(requestName);
 helper.getTestSupportConnectionFactory().setRecording(true);
 Map<String, Object> headers = new HashMap<>();
-headers.put("id", "");// THIS IS REQUIRED
-headers.put("page_size", "");
-headers.put("page_start", "");
-headers.put("page_token", "");
+headers.put("validate_only", "");
 headers.put("credentials", "");
 headers.put("companyId", "");
 headers.put("deviceId", "");
 headers.put("userRole", "");
-headers.put("disablePagination", "");
-headers.put(Exchange.HTTP_METHOD, "GET");
-String body = "";
-Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-jbl.getFtReconciliation", body, headers);
+headers.put(Exchange.HTTP_METHOD, "POST");
+String body = ""; // THIS IS REQUIRED
+Object result = helper.getProducerTemplate().requestBodyAndHeaders("direct-vm:party-payments.createPsopspFtWithChgVat", body, headers);
  Map<String, List<String>> testResults = helper.checkResponse(requestName, result);
 super.checkPaths(testResults);
 }
